9ac271d3d81e598b9149e288dbda741347791ade
Sean Lin <seanlin1016@gmail.com>
2021-05-21 15:40:49 +0800

    Use Lombok annotation for refactoring

--- pet-clinic-data/src/main/java/seanlindev/springframework/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/seanlindev/springframework/springpetclinic/model/PetType.java
@@ -1,21 +1,20 @@
 package seanlindev.springframework.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "petTypes")
 public class PetType extends BaseEntity {
 
     @JoinColumn(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
