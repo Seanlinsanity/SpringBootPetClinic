e3e48058f4035d1f68311f4e04fabd342187357d
Sean Lin <seanlin1016@gmail.com>
2021-06-14 17:15:24 +0800

    Response Json body by ResponseBody annotation

--- pet-clinic-web/src/main/java/seanlindev/springframework/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/seanlindev/springframework/springpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package seanlindev.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import seanlindev.springframework.springpetclinic.model.Vet;
 import seanlindev.springframework.springpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +22,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
